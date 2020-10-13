7ba58a1e5f636a2aa8dfa92e1eb3a433096238b3
ZainNawaz123 <69996704+ZainNawaz123@users.noreply.github.com>
2020-10-13 19:52:39 -0400

    Add files via upload

---
+++ AngryBirdsStage5-master/Box.js
@@ -0,0 +1,28 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("sprites/wood1.png");
+    this.visibility=255
+
+    
+  }
+display() {
+ 
+  
+ 
+
+if(this.body.speed<5.5) {
+super.display();
+}
+else {
+  World.remove(world, this.body)
+  push()
+  this.visibility=this.visibility - 5
+  tint(255, this.visibility)
+  image(this.image,this.body.position.x,this.body.position.y,width,height)
+
+  pop()
+}
+  
+}
+};
